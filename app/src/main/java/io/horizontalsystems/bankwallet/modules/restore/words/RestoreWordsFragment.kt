fefccfffb49505665b7888c90a85b2ecdfec9b40
bakyt <abdrasulov@gmail.com>
2020-11-17 18:09:36 +0600

    Extract method for setting action bar to base class

--- app/src/main/java/io/horizontalsystems/bankwallet/modules/restore/words/RestoreWordsFragment.kt
+++ app/src/main/java/io/horizontalsystems/bankwallet/modules/restore/words/RestoreWordsFragment.kt
@@ -4,7 +4,6 @@ import android.os.Bundle
 import android.text.Editable
 import android.text.TextWatcher
 import android.view.*
-import androidx.appcompat.app.AppCompatActivity
 import androidx.core.os.bundleOf
 import androidx.fragment.app.setFragmentResult
 import androidx.lifecycle.Observer
@@ -43,13 +42,8 @@ class RestoreWordsFragment : BaseFragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-
         setHasOptionsMenu(true)
-
-        (activity as? AppCompatActivity)?.let {
-            it.setSupportActionBar(toolbar)
-            it.supportActionBar?.setDisplayHomeAsUpEnabled(true)
-        }
+        setSupportActionBar(toolbar, true)
 
         val wordsCount = arguments?.getParcelable<RestoreAccountType>(restoreAccountTypeKey) ?: throw Exception("Invalid restore account type")
         val accountTypeTitleRes = arguments?.getInt(titleKey) ?: throw Exception("Invalid title")
